3ed7e0d086f41df217d67e4c4f946a43bf6b02ad
prachik2407 <109075683+prachik2407@users.noreply.github.com>
2023-04-05 13:34:50 +0530

    Add files via upload

---
+++ subnet simulator/ip/src/main/java/com/mycompany/yaship/Yaship.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.yaship;
+
+/**
+ *
+ * @author Yash
+ */
+public class Yaship {
+
+    public static void main(String[] args) {
+        calculator a = new calculator();
+        a.setVisible(true);
+    }
+}
